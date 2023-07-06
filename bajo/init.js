de70295ca23b00534e995875dd7b1cca0882ea90
Ardhi Lukianto <ardhi@lukianto.com>
2023-07-06 21:04:05 +0700

    Refactor

--- bajo/init.js
+++ bajo/init.js
@@ -2,8 +2,8 @@ import EventEmitter2 from 'eventemitter2'
 import collectEvents from '../lib/collect-events.js'
 
 async function init () {
-  const { getPkg, getConfig } = this.bajo.helper
-  const _ = await getPkg('lodash')
+  const { importPackage, getConfig } = this.bajo.helper
+  const _ = await importPackage('lodash')
   const opts = _.pick(getConfig('bajoEmitter'), ['maxListeners', 'verboseMemoryLeak', 'ignoreErrors'])
   opts.wildcard = true
   opts.delimiter = '.'
