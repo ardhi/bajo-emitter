5535eb759456929b97b89381f131759b1fd722c3
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-05 17:04:18 +0700

    Updates

--- bajo/helper/broadcast.js
+++ bajo/helper/broadcast.js
@@ -11,7 +11,7 @@
 async function broadcast ({ msg, from, to }) {
   const { importPkg, log } = this.bajo.helper
   const { find } = await importPkg('lodash-es')
-  if (!find(this.bajoEmitter.broadcastPools, { name: to })) log.error('Unknown broadcast pool \'%s\'', to)
+  if (!find(this.bajoEmitter.broadcastPools, { name: to.name })) log.error('Unknown broadcast pool \'%s\'', to.name)
   else this.bajoEmitter.instance.emit('bajoEmitter.broadcastPool', { msg, from, to })
 }
 
--- bajoEmitter/event/on/broadcastPool.js
+++ bajoEmitter/event/on/broadcastPool.js
@@ -1,16 +1,23 @@
 const broadcastPool = {
   level: 1,
   handler: async function onBroadcastPool ({ msg, from, to }) {
+    const { callHelperOrHandler } = this.bajo.helper
     const { addressSplit } = this.bajoEmitter.helper
     const { importPkg } = this.bajo.helper
     const { get, isFunction, find } = await importPkg('lodash-es')
-    const pool = find(this.bajoEmitter.broadcastPools, { name: to })
-    if (!(pool.from.includes(from) || !pool.from)) return
+    const pool = find(this.bajoEmitter.broadcastPools, p => {
+      return p.name === to.name && (p.from.includes(from) || !p.from)
+    })
+    if (!pool) return
+    let result = msg
+    if (to.transformer) result = await callHelperOrHandler(to.transformer, msg, from, to)
+    if (!result) return
     for (const t of pool.to) {
       const { transport } = addressSplit(t)
-      const handler = get(this, `${transport}.helper.send`)
+      const key = `${transport}.helper.send`
+      const handler = get(this, key)
       if (!isFunction(handler)) continue
-      await handler(msg, { from, to: t })
+      await handler({ msg, from, to: t })
     }
   }
 }
