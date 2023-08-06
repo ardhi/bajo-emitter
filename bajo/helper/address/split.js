b004994f83b9967eb212be68acf6865c14f11516
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-06 09:52:10 +0700

    Updates

--- bajo/helper/address/split.js
+++ bajo/helper/address/split.js
@@ -1,6 +1,7 @@
 function split (address) {
-  const [destination = '', cinfo = ''] = address.split('@')
-  const [connection = '', transport = ''] = cinfo.split('.')
+  if (!address.includes(':')) address = ':' + address
+  const [destination = '', cinfo = ''] = address.split(':')
+  const [connection = '', transport = ''] = cinfo.split('@')
   return { destination, connection, transport }
 }
 
--- bajo/helper/address/verify.js
+++ bajo/helper/address/verify.js
@@ -1,9 +1,9 @@
 async function verify (address) {
   const { addressSplit } = this.bajoEmitter.helper
-  const { importPkg, error } = this.bajo.helper
+  const { importPkg, error, isSet } = this.bajo.helper
   const { isEmpty, find } = await importPkg('lodash-es')
   const { destination, connection, transport } = addressSplit(address)
-  if (isEmpty(destination) || isEmpty(connection) || isEmpty(transport)) throw error('Invalid address \'%s\'', address)
+  if (!isSet(destination) || isEmpty(connection) || isEmpty(transport)) throw error('Invalid address \'%s\'', address)
   if (!this[transport]) throw error('Unknown transport \'%s\' or transport isn\'t loaded yet', transport)
   if (!find(this[transport].connections, { name: connection })) throw error('Unknown connection \'%s@%s\'', connection, transport)
 }
