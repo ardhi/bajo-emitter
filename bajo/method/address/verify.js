e36170d95600db6b890186d89e3264e7d5eaac31
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-15 21:22:03 +0700

    Refactor: new error handling

--- bajo/method/address/verify.js
+++ bajo/method/address/verify.js
@@ -1,10 +1,10 @@
 function verify (address, { skipConnectionCheck = false } = {}) {
-  const { error, isSet } = this.app.bajo
+  const { isSet } = this.app.bajo
   const { isEmpty, find } = this.app.bajo.lib._
   const { subject, connection, plugin } = this.addressSplit(address)
-  if (!isSet(subject) || isEmpty(connection) || isEmpty(plugin)) throw error('Invalid address \'%s\'', address)
-  if (!this.app[plugin]) throw error('Unknown plugin \'%s\' or plugin isn\'t loaded yet', plugin)
-  if (!skipConnectionCheck) if (!find(this.app[plugin].connections, { name: connection })) throw error('Unknown connection \'%s@%s\'', connection, plugin)
+  if (!isSet(subject) || isEmpty(connection) || isEmpty(plugin)) throw this.error('Invalid address \'%s\'', address)
+  if (!this.app[plugin]) throw this.error('Unknown plugin \'%s\' or plugin isn\'t loaded yet', plugin)
+  if (!skipConnectionCheck) if (!find(this.app[plugin].connections, { name: connection })) throw this.error('Unknown connection \'%s@%s\'', connection, plugin)
   return true
 }
 
